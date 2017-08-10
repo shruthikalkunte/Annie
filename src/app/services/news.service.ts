8d52f5d85c3618ee939557194fc54169e34bde89
Shruthi Kalkunte <shruthikalkunte92@gmail.com>
2017-08-10 12:46:00 -0700

    After removing encoding

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -20,48 +20,4 @@ export class NewsService{
         return this.http.get(this.baseUrl+'?limit='+limit)
             .map(res => res.json());
     }
-
-
-//     deleteIt(x) {
-//     // this.data.push(x);
-//     // let index = this.data.indexOf(x);
-//     if(x !== null && x!== undefined){
-//             console.log(x);
-//         this.data=["1"];
-//         console.log(this.data);
-//         let index = this.data.indexOf(x);
-//         console.log(index);
-//     if (index == -1) {
-//         //console.log("inside if");
-//         this.data.splice(index, 1);
-//         //console.log("after if");
-//     } 
-//     console.log("deleted3");
-//     }
-// }
-
-
-deleteIt(x,data){
- if(x !== null && x!== undefined){
-    console.log(x);
-    // this.data=[{x}];
-//    console.log(this.data);
-    let index = data.indexOf({x});
-    console.log(index);
-    if (index !== -1) {
-        console.log("inside if");
-        data.splice(index, 1);
-        console.log("after if");
-    } 
-
-    // for(var i = 0; i < this.data.length; i++) {
-    //   if(this.data[i] == x){
-    //     console.log("inside if")  
-    //     this.data.splice(this.data[i], 1);
-    //     console.log("after deleting");
-    //     console.log(this.data);
-    //   } 
-    // }
- }
-}
 }
